4f8654cd00536e6b47e0e1cdff5cb26b283f3771
Jimmy Orr <jimmyorr@gmail.com>
2026-05-10 09:51:57 -0400

    feat: add eslint configuration and define linting scripts in package.json

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+const globals = require("globals");
+const js = require("@eslint/js");
+
+module.exports = [
+    js.configs.recommended,
+    {
+        files: ["**/*.js"],
+        languageOptions: {
+            ecmaVersion: "latest",
+            sourceType: "script",
+            globals: {
+                ...globals.browser,
+                THREE: "readonly",
+                Sentry: "readonly",
+                firebase: "readonly"
+            }
+        },
+        rules: {
+            "no-unused-vars": "warn",
+            "no-undef": "warn"
+        }
+    },
+    {
+        ignores: ["node_modules/", "vendor/", "www/"]
+    }
+];
