13f39069b33564100992330e1b5d53019d8b1f8d
Jimmy Orr <jimmyorr@gmail.com>
2026-06-04 08:03:18 -0400

    Increase shooting star frequency

--- game.js
+++ game.js
@@ -4195,7 +4195,7 @@ function animate() {
     forceShootingStar
   ) {
     if (!isShootingStarActive) {
-      if (forceShootingStar || Math.random() < delta / 45.0) {
+      if (forceShootingStar || Math.random() < delta / 15.0) {
         forceShootingStar = false;
         isShootingStarActive = true;
         shootingStarProgress = 0;
