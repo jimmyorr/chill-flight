0d5fdaf1695f7c2a943e0ce13cfc62f820df174b
Jimmy Orr <jimmyorr@gmail.com>
2026-06-17 23:07:53 -0400

    refactor: update shader import paths to use .glsl file extensions

--- src/main.js
+++ src/main.js
@@ -3,15 +3,15 @@ import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
 import * as Sentry from '@sentry/browser';
 
 // Import shaders as raw strings via Vite
-import skyVert from './shaders/sky.vert?raw';
-import skyFrag from './shaders/sky.frag?raw';
-import sunMoonVert from './shaders/sunMoon.vert?raw';
-import sunFrag from './shaders/sun.frag?raw';
-import moonFrag from './shaders/moon.frag?raw';
-import sunGlowVert from './shaders/sunGlow.vert?raw';
-import sunGlowFrag from './shaders/sunGlow.frag?raw';
-import rainbowVert from './shaders/rainbow.vert?raw';
-import rainbowFrag from './shaders/rainbow.frag?raw';
+import skyVert from './shaders/sky.vert.glsl?raw';
+import skyFrag from './shaders/sky.frag.glsl?raw';
+import sunMoonVert from './shaders/sunMoon.vert.glsl?raw';
+import sunFrag from './shaders/sun.frag.glsl?raw';
+import moonFrag from './shaders/moon.frag.glsl?raw';
+import sunGlowVert from './shaders/sunGlow.vert.glsl?raw';
+import sunGlowFrag from './shaders/sunGlow.frag.glsl?raw';
+import rainbowVert from './shaders/rainbow.vert.glsl?raw';
+import rainbowFrag from './shaders/rainbow.frag.glsl?raw';
 
 // Expose them globally so existing scripts can still find them
 // We create a new object for THREE because ES module imports are immutable
