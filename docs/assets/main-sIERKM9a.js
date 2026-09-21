c0a6adbb2f416ba0f00235327d56e41b61aa7ca7
Jimmy Orr <jimmyorr@gmail.com>
2026-09-20 22:22:13 -0400

    web hotfix for phantom highway issue

--- docs/assets/main-sIERKM9a.js
+++
@@ -1,1 +0,0 @@
-import"./main-B5s42Gqc.js";/* empty css              */document.querySelectorAll(`.version-tag`).forEach(e=>{e.textContent=`v0.9.23`});var e=document.getElementById(`debug-version-info`);e&&(e.textContent=`v0.9.23 (3ee2d31*)`);
