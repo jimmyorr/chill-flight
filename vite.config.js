34ab1ff7cda499a1a78fd9b54c57b1da091106b6
Jimmy Orr <jimmyorr@gmail.com>
2026-09-25 20:25:12 -0400

    chore: increase chunk size warning limit in vite config

--- vite.config.js
+++ vite.config.js
@@ -74,6 +74,7 @@ export default defineConfig({
   build: {
     outDir: 'docs',
     emptyOutDir: true,
+    chunkSizeWarningLimit: 1200,
     rollupOptions: {
       input: {
         main: 'index.html',
